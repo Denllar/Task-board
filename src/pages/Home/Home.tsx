b8d6d8972aa422e502b6c09111fa44311d3c1d7a
Denis Gabbasov <denisgabbasov06.09.2004@gmail.com>
2024-03-09 12:09:08 +0500

    Обновление папки компоненты

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { setCount } from '../../redux/slices/firstSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hook';
 
-import Navigation from '../../components/Navigation';
+import Navigation from '../../components/Navigation/Navigation';
 
 const Home: React.FC = () => {
     const dispatch = useAppDispatch();
