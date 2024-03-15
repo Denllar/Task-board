97e02b60cbd0122ead192aceb48e9bd39240643e
Denis Gabbasov <denisgabbasov06.09.2004@gmail.com>
2024-03-15 23:25:10 +0500

    Исправление карточек

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,37 +3,38 @@ import style from "./Card.module.scss"
 
 
 const Card: React.FC = () => {
-  return (
-    <div className={style.wrapper}>
-        <div>
-            <div className={style.header}>
-                <h2>Web Task</h2>
-                <img width={20} src="../../public/ellipsis.png" alt="настройки"/>
-            </div>
-            
-            <p className={style.main}>Lorem ipsum dolor sit</p>
+    const [count, setCount] = React.useState(0);
+    return (
+        <div className={style.wrapper}>
+            <div>
+                <div className={style.header}>
+                    <h2>Web Task</h2>
+                    <img width={20} src="../../public/ellipsis.png" alt="настройки" />
+                </div>
 
-            <div className={style.progress}>
-                <progress value="75" max="100"/>
-                <p>34%</p>
-            </div>
+                <p className={style.main}>Lorem ipsum dolor sit</p>
 
-            <div className={style.footer}>
-                <p>Иконки</p>
-                <div className={style.clipMeassage}>
-                    <div className={style.clip}>
-                        <img width={10} src="../../public/clip.png" alt="закреплено"/>
-                        <p>3</p>
-                    </div>
-                    <div className={style.message}>
-                        <img width={10} style={{opacity: 0.6}} src="../../public/message2.png" alt="сообщение"/>
-                        <p>5</p>
+                <div className={style.progress}>
+                    <progress onClick={()=>setCount(prev=>prev+7)} value={count} max="100" />
+                    <p>{count}%</p>
+                </div>
+
+                <div className={style.footer}>
+                    <p>Иконки</p>
+                    <div className={style.clipMeassage}>
+                        <div className={style.clip}>
+                            <img width={10} src="../../public/clip.png" alt="закреплено" />
+                            <p>3</p>
+                        </div>
+                        <div className={style.message}>
+                            <img width={10} style={{ opacity: 0.6 }} src="../../public/message2.png" alt="сообщение" />
+                            <p>5</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Card
